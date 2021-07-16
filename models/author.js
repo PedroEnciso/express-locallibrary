630b7871e146204b2eb92b82b9281bc363da9183
Pedro Enciso <penciso@ucsd.edu>
2021-07-15 19:37:54 -0700

    created schema files, populated author.js

---
+++ models/author.js
@@ -0,0 +1,40 @@
+let mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// virtual for author's full name
+// virtuals do not get stored in the database, but are helper methods to combine data from a data entry, in this case authors
+AuthorSchema.virtual("name").get(function () {
+  return this.family_name + ", " + this.first_name;
+});
+
+// virtual for author's lifespan
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocalString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string = +DateTime.fromJSDate(this.date_of_death).toLocalString(
+      DateTime.DATE.MED
+    );
+  }
+  return lifetime_string;
+});
+
+// virtual fro author's URL
+AuthorSchema.virtual("url").get(function () {
+  return "/catalog/author/" + this._id;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
